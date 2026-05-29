b0e5915717b1237a467f42850492dcc425e1c06a
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:02:54 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 19,
+  "doc_id": "frus1989-92v18",
+  "title": "1989\u20131992, Volume XVIII, Japan; Korea",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1989-92v18",
+  "repo_slug": "frus-1989-92v18-assist",
+  "title_ascii": "1989-1992, Volume XVIII, Japan; Korea",
+  "pages_url": "https://therealjameswilson.github.io/frus-1989-92v18-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1989-92v18-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=XVIII+OR+Japan+OR+Korea&from=1989&to=1992&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
